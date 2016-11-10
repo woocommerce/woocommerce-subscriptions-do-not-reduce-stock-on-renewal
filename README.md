0d4c7fcabbe8cf08c18d29cc25df3bef63dbdb9c
Brent Shepherd <thenbrent@gmail.com>
2016-11-10 11:56:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# WooCommerce Subscriptions - Do Not Reduce Stock on Renewal
+
+Using [WooCommerce Subscriptions](https://woocommerce.com/products/woocommerce-subscriptions/)? Want to reduce stock for only the initial purchase of subscription products?
+
+Install and activate this plugin.
+
+By default, WooCommerce reduces stock for any order containing a product. This means stock will be reduced for both the initial purchase of a subscription product and all [renewal orders](https://docs.woocommerce.com/document/subscriptions/renewal-process/). This extension stops stock being reduced for renewal orders.
+
+> Note: this plugin was originally made available [via a Gist](https://gist.github.com/thenbrent/968526e7ae01308eaabc/). It's been moved into this repository because of the discussion and popularity of that gist.
+
+## Requirements
+
+This plugin requires WooCommerce 2.4 or newer. It will work with any version of Subscriptions, even v1.5, but _please_ update to Subscriptions 2.0 or newer. :pray:
+
+## Installation
+
+To install:
+
+1. Download the latest version of the plugin [here](https://github.com/Prospress/woocommerce-subscriptions-do-not-reduce-stock-on-renewal/archive/master.zip)
+1. Go to **Plugins > Add New > Upload** administration screen on your WordPress site
+1. Select the ZIP file you just downloaded
+1. Click **Install Now**
+1. Click **Activate**
+
+## Reporting Issues
+
+If you find an problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/woocommerce-subscriptions-do-not-reduce-stock-on-renewal/issues/new).
+
+---
+
+<p align="center">
+	<a href="https://prospress.com/">
+		<img src="https://cloud.githubusercontent.com/assets/235523/11986380/bb6a0958-a983-11e5-8e9b-b9781d37c64a.png" width="160">
+	</a>
+</p>
---
+++ woocommerce-do-not-reduce-renewal-stock.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Subscriptions - Do Not Reduce Stock on Renewal
+ * Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-do-not-reduce-stock-on-renewal/
+ * Description: By default, WooCommerce reduces stock for any order containing a product. This means stock will be reduced for both the initial purchase of a subscription product and all renewal orders. This extension stops stock being reduced for renewal order payments so that stock is only reduced on the initial purchase. Requires WooCommerce 2.4 or newer and Subscriptiosn 2.0 or newer.
+ * Author: Prospress Inc.
+ * Author URI: https://prospress.com/
+ * License: GPLv2
+ *
+ * GitHub Plugin URI: Prospress/woocommerce-subscriptions-do-not-reduce-stock-on-renewal
+ * GitHub Branch: master
+ *
+ * Copyright 2016 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		WooCommerce Subscriptions - Do Not Reduce Stock on Renewal
+ * @author		Brent Shepherd
+ * @since		1.0
+*/
+
+function wcs_do_not_reduce_renewal_stock( $reduce_stock, $order ) {
+
+	if ( function_exists( 'wcs_order_contains_renewal' ) && wcs_order_contains_renewal( $order ) ) { // Subscriptions v2.0+
+		$reduce_stock = false;
+	} elseif ( class_exists( 'WC_Subscriptions_Renewal_Order' ) && WC_Subscriptions_Renewal_Order::is_renewal( $order ) ) {
+		$reduce_stock = false;
+	}
+
+	return $reduce_stock;
+}
+add_filter( 'woocommerce_can_reduce_order_stock', 'wcs_do_not_reduce_renewal_stock', 10, 2 );
